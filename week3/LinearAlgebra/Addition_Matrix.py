595194347afe5da3d4ba6a80ccee8df7805c5fd1
meghakoushik <koushikmegha011@gmail.com>
2019-03-20 17:56:48 +0530

    Program to perform addition matrix.

--- week3/LinearAlgebra/Addition_Matrix.py
+++ week3/LinearAlgebra/Addition_Matrix.py
@@ -4,13 +4,13 @@ from week3.Utility.Utilclass1 import Util
 class Addition(Util):
     obj = Util()
 
-    def __init__(self) :
+    def __init__(self):
         super(Addition, self).__init__()
 
     def display_matrix(self):
         result = obj.addition_matrix(self.first_matrix, self.second_matrix)
         print('addition matrix')
-        for value in result :
+        for value in result:
             print(value)
 
 
